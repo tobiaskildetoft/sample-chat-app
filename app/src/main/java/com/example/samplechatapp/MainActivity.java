e49df31e800bb1107d7413e8b87cdde210741ebf
mothki <mona.kildetoft@gmail.com>
2019-02-22 10:28:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/samplechatapp/MainActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Sample Chat App for application at House of Code.
+ * MainActivity will host a list of chatrooms to be fetched from Firebase database.
+ * Main entry point for the app from a launcher will be the SplashScreen Activity which
+ * displays a splash screen while MainActivity loads, in order to allow fade between the two.
+ */
+
+package com.example.samplechatapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    // member variables for the Views in the layout
+
+
+    // member variables for Firebase objects
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
